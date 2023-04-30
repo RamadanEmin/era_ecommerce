88e806f285e23b9ff76752af2c635fca83377171
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-30 12:28:56 +0300

    Added "Footer banner" component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { client } from '../lib/client';
-import { Product, HeroBanner } from '../components';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => {
     return (
@@ -17,6 +17,7 @@ const Home = ({ products, bannerData }) => {
                 {products?.map((product) => <Product key={product._id} product={product} />)}
             </div>
 
+            <FooterBanner footerBanner={bannerData.length && bannerData[0]}/>
         </>
     );
 };
