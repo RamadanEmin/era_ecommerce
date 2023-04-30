8faee4d89231d45112d2254537530ed03e3da03d
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-30 13:09:12 +0300

    Added Product details page

---
+++ pages/product/[slug].js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineStar, AiFillStar } from 'react-icons/ai';
+
+import { client, urlFor } from '../../lib/client';
+
+const ProductDetails = ({ product, products }) => {
+    const { image, name, details } = product;
+    const [index, setIndex] = useState(0);
+
+    return (
+        <div>
+            <div className="product-detail-container">
+                <div>
+                    <div className="image-container">
+                        <img src={urlFor(image && image[index])} className="product-detail-image" />
+                    </div>
+                </div>
+                <div className="product-detail-desc">
+                    <h1>{name}</h1>
+                    <div className="reviews">
+                        <div>
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiFillStar />
+                            <AiOutlineStar />
+                        </div>
+                        <p>(20)</p>
+                    </div>
+                    <h4>Details:</h4>
+                    <p>{details}</p>
+                    <p className="price">0 лв</p>
+                    <div className="quantity">
+                        <h3>Quantity:</h3>
+                        <p className="quantity-desc">
+                            <span className="minus" ><AiOutlineMinus /></span>
+                            <span className="num">0</span>
+                            <span className="plus" ><AiOutlinePlus /></span>
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button type="button" className="add-to-cart" >Add to Cart</button>
+                        <button type="button" className="buy-now" >Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+
+    const products = await client.fetch(query);
+
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }));
+
+    return {
+        paths,
+        fallback: 'blocking'
+    };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productQuery = '*[_type == "product"]';
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productQuery);
+
+    return {
+        props: { product, products }
+    };
+};
+
+export default ProductDetails;
