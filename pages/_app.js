9ddff1d4022ad31929a37d6a08e09e77a17156fd
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-05-01 08:57:07 +0300

    Added StateContext and increase/decrease quantity functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 
 import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
     return (
+        <StateContext>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
+        </StateContext>
     );
 }
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineStar, AiFillStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
-    const { image, name, details } = product;
+    const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
+    const { qty, incQty, decQty} = useStateContext();
 
     return (
         <div>
@@ -43,9 +46,9 @@ const ProductDetails = ({ product, products }) => {
                     <div className="quantity">
                         <h3>Quantity:</h3>
                         <p className="quantity-desc">
-                            <span className="minus" ><AiOutlineMinus /></span>
-                            <span className="num">0</span>
-                            <span className="plus" ><AiOutlinePlus /></span>
+                            <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+                            <span className="num">{qty}</span>
+                            <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
                         </p>
                     </div>
                     <div className="buttons">
